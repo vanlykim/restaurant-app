211b15f44a8862d22750bbc2dafaf91f9adac451
Vanly KIM <kim.sovanly@gmail.com>
2022-05-26 18:06:31 +0700

    Add restaurant controller functionalities

--- backend/controllers/restaurant.js
+++ backend/controllers/restaurant.js
@@ -1,10 +1,27 @@
 const asyncHandler = require("express-async-handler");
+const { Op } = require("sequelize");
+const { Restaurant, OpeningHour } = require("../models");
 
 // @desc    Get restaurants
 // @route   GET /api/restaurants
 // @access  Public
 const getRestaurants = asyncHandler(async (req, res) => {
-  res.send("Get restaurants");
+  const { q, w, t } = req.query;
+  const restaurants = await Restaurant.findAll({
+    where: {
+      name: { [Op.iLike]: `%${q}%` },
+      $weekday$: w,
+      $open$: { [Op.lte]: t },
+      $close$: { [Op.gte]: t },
+    },
+    include: [
+      {
+        model: OpeningHour,
+        required: false,
+      },
+    ],
+  });
+  res.status(200).json(restaurants);
 });
 
 module.exports = {
