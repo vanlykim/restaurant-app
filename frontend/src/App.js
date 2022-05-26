7222117f77b7ad677d49d36d1f504e697a01b814
Vanly KIM <kim.sovanly@gmail.com>
2022-05-26 19:32:31 +0700

    Initialize frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container-fluid'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
