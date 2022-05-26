c99c385256163e8e2f99b79f1e2390b40f0fc504
Vanly KIM <kim.sovanly@gmail.com>
2022-05-26 23:59:22 +0700

    Prapare deploy

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# restaurant-app
-Restaurant app for Glints
+# Restaurant App
+
+This is the restaurant app for Glints.
+
+## Usage
+
+Rename the .env.example to .env and add your environment varaibles
+
+### Install dependencies
+
+```
+# Backend dependencies
+npm install
+
+# Frontend dependencies
+cd frontend
+npm install
+```
+
+### Generate data
+
+```
+npm run data
+```
+
+### Run migrations
+
+```
+npx sequelize-cli db:migrate
+```
+
+### Run seeds
+
+```
+npx sequelize-cli db:seed:all
+```
+
+### Run both server and client at the same time
+
+```
+npm run dev
+```
+
+### Run server
+
+```
+npm run server
+```
+
+### Run client
+
+```
+npm run client
+```
+
+## Demo
+
+https://restaurant-app-glints.herokuapp.com/
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const path = require("path");
 const {
   userRoute,
   restaurantRoute,
@@ -9,8 +10,13 @@ const {
   restaurantCollectionRoute,
 } = require("./routes");
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const { sequelize } = require("./models");
 const port = process.env.PORT || 5000;
 const app = express();
+
+sequelize.sync().then(() => {
+  console.log("Postgres connected!");
+});
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +25,20 @@ app.use("/api/users", userRoute);
 app.use("/api/restaurants", restaurantRoute);
 app.use("/api/collections", collectionRoute);
 app.use("/api/restaurant-collections", restaurantCollectionRoute);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
